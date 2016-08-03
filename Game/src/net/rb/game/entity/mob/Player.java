f0c14ceeae4a08ae08da1c8bbceae31e9be16870
Richard_Bergqvist <richard.bergqvist2@gmail.com>
2016-08-03 16:56:04 +0200

    Updated
    Removed old player animation code and replaced it with animated sprites.

--- Game/src/net/rb/game/entity/mob/Player.java
+++ Game/src/net/rb/game/entity/mob/Player.java
@@ -1,20 +1,34 @@
 package net.rb.game.entity.mob;
 
+import net.rb.game.Game;
+import net.rb.game.entity.projectile.Projectile;
+import net.rb.game.entity.projectile.WizardProjectile;
+import net.rb.game.graphics.AnimatedSprite;
 import net.rb.game.graphics.Screen;
 import net.rb.game.graphics.Sprite;
+import net.rb.game.graphics.SpriteSheet;
 import net.rb.game.input.Keyboard;
+import net.rb.game.input.Mouse;
 
 public class Player extends Mob
 {
 	private Keyboard input;
 	private Sprite sprite;
-	private int anim = 0;
 	private boolean walking = false;
+	private AnimatedSprite down  = new AnimatedSprite(SpriteSheet.player_down, 32, 32, 3);
+	private AnimatedSprite up    = new AnimatedSprite(SpriteSheet.player_up, 32, 32, 3);
+	private AnimatedSprite left  = new AnimatedSprite(SpriteSheet.player_left, 32, 32, 3);
+	private AnimatedSprite right = new AnimatedSprite(SpriteSheet.player_right, 32, 32, 3);
+	
+	private AnimatedSprite playerSprite = down;
+	
+	private int fireRate = 0;
 	
 	public Player(Keyboard input )
 	{
 		this.input = input;
-		sprite = Sprite.player_forward;
+		this.fireRate = WizardProjectile.FIRE_RATE;
+		this.playerSprite = down;
 	}
 	
 	public Player(int x, int y, Keyboard input)
@@ -22,18 +36,41 @@ public class Player extends Mob
 		this.x = x;
 		this.y = y;
 		this.input = input;
-		sprite = Sprite.player_forward;
+		this.fireRate = WizardProjectile.FIRE_RATE;
+		this.playerSprite = down;
 	}
 	
 	public void update()
 	{
+		if (walking) playerSprite.update();
+		else playerSprite.setFrame(0);
+		if (fireRate > 0) fireRate--;
 		int xa = 0, ya = 0;
-		if (anim < 7500) anim++;
-		else anim = 0;
-		if (input.up) ya--;
-		if (input.down) ya++;
-		if (input.left) xa--;
-		if (input.right) xa++;
+	
+		playerSprite.setFrameRate(8);
+		if (input.up)
+		{
+			playerSprite = up;
+			ya--;
+		}
+		
+		if (input.down)
+		{
+			playerSprite = down;
+			ya++;
+		}
+		
+		if (input.left)
+		{
+			playerSprite = left;
+			xa--;
+		}
+		
+		if (input.right)
+		{
+			playerSprite = right;
+			xa++;
+		}
 		
 		if (xa != 0 || ya != 0)
 		{
@@ -42,65 +79,35 @@ public class Player extends Mob
 		} else {
 			walking = false;
 		}
+		
+		clear();
+		updateShooting();
+	}
+	
+	private void clear()
+	{
+		for (int i = 0; i < level.getProjectiles().size(); i++)
+		{
+			Projectile p = level.getProjectiles().get(i);
+			if (p.isRemoved()) level.getProjectiles().remove(i);
+		}
+	}
+
+	private void updateShooting()
+	{
+		if (Mouse.getButton() == 1 && fireRate <= 0)
+		{
+			double dx = Mouse.getX() - Game.getWindowWidth() / 2;
+			double dy = Mouse.getY() - Game.getWindowHeight() / 2;
+			double dir = Math.atan2(dy, dx);
+			shoot(x, y, dir);
+			fireRate = WizardProjectile.FIRE_RATE;
+		}
 	}
 	
 	public void render(Screen screen)
 	{
-		int flip = 0;
-		if (dir == 0)
-		{
-			sprite = Sprite.player_forward;
-			if (walking)
-			{
-				if (anim % 20 > 10)
-				{
-					sprite = Sprite.player_forward_1;
-				} else {
-					sprite = Sprite.player_forward_2;
-				}
-			}
-		}
-		if (dir == 1)
-		{
-			sprite = Sprite.player_side;
-			if (walking)
-			{
-				if (anim % 20 > 10)
-				{
-					sprite = Sprite.player_side_1;
-				} else {
-					sprite = Sprite.player_side_2;
-				}
-			}
-		}
-		if (dir == 2)
-		{
-			sprite = Sprite.player_back;
-			if (walking)
-			{
-				if (anim % 20 > 10)
-				{
-					sprite = Sprite.player_back_1;
-				} else {
-					sprite = Sprite.player_back_2;
-				}
-			}
-		}
-		if (dir == 3)
-		{
-			sprite = Sprite.player_side;
-			flip = 1;
-			if (walking)
-			{
-				if (anim % 20 > 10)
-				{
-					sprite = Sprite.player_side_1;
-				} else {
-					sprite = Sprite.player_side_2;
-				}
-			}
-		}
-		
-		screen.renderPlayer(x - 16, y - 16, sprite, flip);
+		sprite = playerSprite.getSprite();
+		screen.renderPlayer(x - 16, y - 16, sprite);
 	}
 }
