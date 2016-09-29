a054826c82f09628ba691d286cb46e8cefaa22ba
Richard Bergqvist <richard.bergqvist2@gmail.com>
2016-09-29 11:09:44 +0200

    Updated to 6.1!

--- Tacitum/src/net/rb/tacitum/level/MartinusStomachLevel.java
+++ Tacitum/src/net/rb/tacitum/level/MartinusStomachLevel.java
@@ -5,23 +5,9 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-import net.rb.tacitum.graphics.Font;
-import net.rb.tacitum.graphics.Screen;
-
-public class SpaceLevel extends Level {
-	public final static int col_space_floor = 0xFF808080;
-	public final static int col_space_wall = 0xFF404040;
-	public static final int col_space_wall1 = 0xFF232323;
-	public static final int col_space_star = 0xFFFFD800;
-	
-	public SpaceLevel(String path) {
+public class MartinusStomachLevel extends Level {
+	public MartinusStomachLevel(String path) {
 		super(path);
-	}
-	
-	public static void renderText(Screen screen) {
-		Font font = new Font();
-		font.render(63, 103, "LIANG", screen);
-		
 	}
 	
 	protected void loadLevel(String path) {
